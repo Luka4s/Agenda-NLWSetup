5ed4075bdd7720f3f383b41719c1fc7fba4fa455
Lucas Rodrigues <lucas.rodrigues28012003@gmail.com>
2023-01-25 16:45:53 -0300

    Correção Js

--- Script.js
+++ Script.js
@@ -5,8 +5,8 @@ const button = document.querySelector("header button")
 button.addEventListener("click", add)
 form.addEventListener("change", save)
 function add() {
-  /*  const today = new Date().toLocaleDateString("pt-br").slice(0, -5) */
-  const today = "01/01"
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
   const dayExists = nlwSetup.dayExists(today)
   if (dayExists) {
     alert("Dia já incluso❌")
